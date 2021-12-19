db821a78ecc153c9dd0da5fc8fb0b2d67d7f4243
Sean Gallagher <seangllghr@gmail.com>
2021-12-18 21:29:05 -0500

    shift to org-publish as site generator
    Since I already started writing my narratives in org and was planning on
    using org-mode's publishing features to push to both this site and
    LaTeX/PDF, it makes sense to publish directly to static HTML and
    eliminate the extra Markdown/Jekyll step.

---
+++ publish.el
@@ -0,0 +1,72 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(package-install 'htmlize)
+
+(require 'ox-publish)
+
+(setq user-full-name "Sean Gallagher"
+      user-mail-address "seangllghr@gmail.com")
+
+(setq
+ simplecss "<link
+    href=\"https://cdn.simplecss.org/simple.min.css\"
+    rel=\"stylesheet\"
+    type=\"text/css\"
+/>"
+ header "<h1>Sean Gallagher</h1>
+<nav>
+  <a href=\"/\">Home</a>
+  <a href=\"/stocks.html\">The STOCKS Application</a>
+  <a href=\"/build.html\">How it's Made</a>
+</nav>")
+
+(setq org-html-head simplecss
+      org-html-preamble header
+      org-html-postamble "<p>&copy;&thinsp;2021 %a<br>%e</p>")
+
+(setq org-publish-project-alist
+      (list
+       (list "seangllghr.github.io:content"
+             :language "en"
+             :base-directory "./src"
+             :recursive t
+             :base-extension "org"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html
+             :headline-levels 5
+             :html-divs '((preamble "header" "header")
+                          (content "main" "content")
+                          (postamble "footer" "footer"))
+             :html-doctype "html5"
+             :html-head-include-default-style nil
+             :html-head-include-scripts nil
+             :html-html5-fancy t
+             :html-indent nil
+             :html-validation-link nil
+             :section-numbers nil
+             :with-date nil
+             :with-author t
+             :with-title nil
+             :with-toc nil)
+       (list "seangllghr.github.io:static"
+             :base-directory "./src"
+             :recursive t
+             :base-extension "css\\|jpg\\|gif\\|png\\|svg"
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment)
+       (list "seangllghr.github.io:archive"
+             :base-directory "./src"
+             :recursive t
+             :base-extension "org"
+             :exclude "\\(build\\)\\|\\(index\\).org"
+             :publishing-directory "./archive"
+             :publishing-function 'org-latex-publish-to-latex)))
+
+(org-publish-all t)
+(message "Build complete")
